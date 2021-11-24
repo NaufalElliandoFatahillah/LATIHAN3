9c2ce2972ec2cebc40187e12b6a27bb980ef50b2
Naufal Elliando Fatahillah <92315859+NaufalElliandoFatahillah@users.noreply.github.com>
2021-11-24 11:00:21 +0700

    Add files via upload

---
+++ TUGAS MATEMATIKA.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int A ;
+    int B ;
+    
+    cin >> A ;
+	cin >> B ;
+    
+    cout << endl ;
+    
+    cout << A + B << endl ;
+    cout << A - B << endl ;
+    cout << A * B << endl ;
+    
+    return 0;
+}
